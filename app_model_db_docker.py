999b9ce6ba56364ecac054c3fdbbbe1ce76dd2eb
GabrielaRomina <gabrielarominalupas@gmail.com>
2023-07-11 21:46:19 +0200

    docker

--- app_model_db_docker.py
+++ app_model_db_docker.py
@@ -4,12 +4,9 @@ import pickle
 import pandas as pd
 import sqlite3
 
-os.chdir(os.path.dirname(__file__))
-
 app = Flask(__name__)
-app.config['DEBUG'] = True
 
-@app.route("/", methods=['GET'])
+@app.route("/")
 def hello():
     return "Bienvenido a mi API del modelo advertising"
 
@@ -105,5 +102,6 @@ def retrain_model():
 
     return 'Model retrained and updated successfully'
 
-app.run()
+if __name__ == '__main__':
+  app.run(debug = True, host = '0.0.0.0', port=os.environ.get("PORT", 5000))
 
